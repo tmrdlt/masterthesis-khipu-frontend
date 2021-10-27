4c5081171f35985b0c355559a905e4e8f3b04473
Timo Erdelt <ti.erdelt@gmail.com>
2021-10-27 15:38:21 +0200

    Remove focus outline for add buttons

--- components/buttons/add-button.tsx
+++ components/buttons/add-button.tsx
@@ -12,7 +12,7 @@ const AddButton = ({ addString, addFunction }: AddButtonProps): JSX.Element => {
       onClick={() => {
         addFunction()
       }}
-      className="w-full inline-flex justify-center rounded-md border border-gray-300 shadow-sm px-4 py-2 bg-white text-sm font-medium text-gray-700 hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+      className="w-full inline-flex justify-center rounded-md border border-gray-300 shadow-sm px-4 py-2 bg-white text-sm font-medium text-gray-700 hover:bg-gray-50 focus:outline-none"
     >
       Add {addString}
     </button>
--- components/modals/create-workflowlist-modal.tsx
+++ components/modals/create-workflowlist-modal.tsx
@@ -191,7 +191,7 @@ const CreateWorkflowListModal = ({
                     onClick={() => {
                       addKanbanBoardColumns()
                     }}
-                    className="w-full inline-flex justify-center rounded-md border border-gray-300 shadow-sm px-4 py-2 bg-white text-sm font-medium text-gray-700 hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+                    className="w-full inline-flex justify-center rounded-md border border-gray-300 shadow-sm px-4 py-2 bg-white text-sm font-medium text-gray-700 hover:bg-gray-50 focus:outline-none"
                   >
                     Make Kanban Board
                   </button>
