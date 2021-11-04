6750c9551335934416550da006053ed0ec594222
Timo Erdelt <ti.erdelt@gmail.com>
2021-11-04 14:45:11 +0100

    Clickable area of drop button bigger, correct z index of cancel move button

--- components/buttons/drop-button.tsx
+++ components/buttons/drop-button.tsx
@@ -9,23 +9,18 @@ interface IDropButtonProps {
 const DropButton = ({ workflowList, moveWorkflowList }: IDropButtonProps): JSX.Element => {
   return (
     <div className="z-40 relative transition-all">
-        <div className="group min-w-min max-w-min">
-            <div className="h-5 w-20 bg-transparent group-hover:bg-white group-hover:bg-opacity-20 text-gray-600 rounded-t border-l-2 border-r-2 border-dashed rounded border-t-2">
-
-            </div>
-            <button
-                type="button"
-                onClick={() => {
-                    moveWorkflowList(workflowList)
-                }}
-                className="flex items-center bg-transparent group-hover:bg-white group-hover:bg-opacity-20 justify-center border border-dashed rounded border-2 text-white w-[20rem] h-[4rem]"
-            >
-                <div className="text-sm">
-                    Move to {workflowList ? `"${workflowList.title}"` : 'root'}
-                </div>
-            </button>
+      <button
+        type="button"
+        onClick={() => {
+          moveWorkflowList(workflowList)
+        }}
+        className="group"
+      >
+        <div className="h-5 w-20 bg-transparent group-hover:bg-white group-hover:bg-opacity-20 text-gray-600 rounded-t border-l-2 border-r-2 border-dashed rounded border-t-2"/>
+        <div className="flex items-center bg-transparent group-hover:bg-white group-hover:bg-opacity-20 justify-center border border-dashed rounded border-2 text-white w-[20rem] h-[4rem]">
+          <div className="text-sm">Move to {workflowList ? `"${workflowList.title}"` : 'root'}</div>
         </div>
-
+      </button>
     </div>
   )
 }
--- components/modals/move-workflowlist-modal.tsx
+++ components/modals/move-workflowlist-modal.tsx
@@ -19,7 +19,7 @@ const MoveWorkflowListModal = ({
             closeModal()
             selectWorkflowListToMove(null)
           }}
-          className="fixed z-40 mt-4 h-8 w-auto inline-flex items-center justify-center rounded-md border border-gray-300 shadow-sm px-4 py-2 bg-white text-base font-medium text-sm text-gray-700 hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 sm:w-auto sm:text-sm"
+          className="fixed z-50 mt-4 h-8 w-auto inline-flex items-center justify-center rounded-md border border-gray-300 shadow-sm px-4 py-2 bg-white text-base font-medium text-sm text-gray-700 hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 sm:w-auto sm:text-sm"
         >
           Cancel moving
         </button>
